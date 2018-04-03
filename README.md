f5ffef274ca3e5203487d1762f9116b4eddaff05
jonaszleflour <jonasz.leflour@ensicaen.fr>
2018-04-03 07:50:27 +0000

    Added multiplayer functionalities. Todo : implement connecting+identifier attribution steps

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Impact VR - master
+|# Impact VR - master
 ## What we want
 
 We want a **seller** to be able to control where the **client** will be in a VR appartment. For that, we need a web site, where the **seller** will be able to see where the **client** is located in realtime and be able to teleport the **client**.
@@ -32,3 +32,118 @@ Backend is going to use nodejs as well as **express** to create a simple web ser
 * type `nodemon` instead `npm start`
 * -Heroku- 
 * Source tree app (google it)
+
+
+### PlayCanvas - Javascript Code
+------- Send positional data (X, Y, Z + Rotation(Z)) to the Seller - PlayCanvast -------
+
+var Sfasaf = pc.createScript('sfasaf');
+
+// initialize code called once per entity
+Sfasaf.prototype.initialize = function() {
+    console.log('running init');
+};
+
+var delay = 250; //250
+var currentDelay = 0;
+
+// update code called every frame
+Sfasaf.prototype.update = function(dt) {
+    if(currentDelay === 0 ) {
+        currentDelay = Date.now();
+    }
+    
+    var self = this;
+    
+    if(Date.now() > delay + currentDelay) {
+        console.log('run script');
+        
+        currentDelay = 0;
+        
+        $.get( "http://localhost:3000/client-infos", function( data ) { //"http://localhost:3000/client-infos" https://aqueous-plateau-58732.herokuapp.com/client-infos
+          console.log(data);
+            var quat = new pc.Quat().setFromEulerAngles(0, (data.RotZ)-180, 0);
+            
+            self.entity.setPosition(-(data.Y), 0, data.X);
+            self.entity.setRotation(quat.invert());
+        });
+    }
+    
+};
+
+
+------- On Click mouse to teleport VR character (Send data to Client UE4) -------
+
+//var ClickTeleport = pc.createScript('clickTeleport');
+var ClickTeleport = pc.createScript('clickTeleport'); 
+   
+ClickTeleport.attributes.add('x', {
+    type: 'number'
+});
+
+ClickTeleport.attributes.add('y', {
+    type: 'number'
+});
+
+ClickTeleport.attributes.add('z', {
+    type: 'number'
+});
+
+ClickTeleport.attributes.add('rotation', {
+    type: 'number'
+});
+
+// initialize code called once per entity
+
+
+ClickTeleport.prototype.initialize = function() {
+    
+    this.app.mouse.on(pc.EVENT_MOUSEDOWN, this.onMouseDown, this);
+};
+
+
+
+/*PickerFramebuffer.prototype = {
+        // Called once after all resources are loaded and before the first update
+        initialize: function () {
+            app.mouse.on(pc.input.EVENT_MOUSEDOWN, this.onSelect, this);
+        },*/
+
+
+
+
+// update code called every frame
+ClickTeleport.prototype.update = function(dt) {
+    
+};
+
+ClickTeleport.prototype.onMouseDown = function (event) {
+    
+    console.log(event);
+    
+    // If the left mouse button is pressed, change the cube color to red
+    if (event.button === pc.MOUSEBUTTON_LEFT) {
+        // this.entity.model.meshInstances[0].material = this.redMaterial.resource;
+        console.log('clicked this');
+        
+        // this whole object will be returned when polling commands from unreal
+
+         var data = {
+            command: "Teleport", // this is the variable to check in unreal when getting the command from server
+            x: this.x,
+            y: this.y,
+            z: this.z,
+            rotation: this.rotation
+        };
+        
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/seller",
+            data: data,
+            dataType: "JSON" 
+        });
+    }
+}; 
+
+
+
--- index.js
+++ index.js
@@ -7,20 +7,74 @@ const app        = express()
 app.use(BodyParser.urlencoded({ extended: false }))
 app.use(BodyParser.json())
 
+
 app.set('port', (process.env.PORT || 3000));
+
 
 let Commands = []
 let ClientCurrentPosition = {}
 
 
-app.get('/client-infos', function (req, res) {   
-  res.set('Access-Control-Allow-Origin', '*');
-  return res.status(200).json(ClientCurrentPosition)
+//multiplayer
+let disconectCheckDelay=10;
+let disconectTimeout=3;
+let vr_side_apps = new Map();
+
+function detectTimeout() {
+   for (let [ip, data] of vr_side_apps){
+	   data.timeSinceUpdate++
+	   if(data.timeSinceUpdate > disconectTimeout){
+		   console.log("VR APP DISCONNECTED:", ip)
+		   vr_side_apps.delete(ip)
+	   }
+   }
+}
+setInterval(detectTimeout, disconectCheckDelay*1000);
+
+
+function getIp(req){
+	return req.headers['x-forwarded-for'] || 
+         req.connection.remoteAddress || 
+         req.socket.remoteAddress || 
+         req.connection.socket.remoteAddress;
+}
+
+//return current position (for the specified app)
+app.get('/client-infos', function (req, res) {
+	var appID = req.body.appid;
+	console.log("CLIENT INFO : APPID=",req.body);
+	if(vr_side_apps.has(appID)){
+		res.set('Access-Control-Allow-Origin', '*')
+		return res.status(200).json(vr_side_apps[appID].ClientCurrentPosition)
+	}
+	return res.status(404)
 })
 
 
-app.post('/client-infos', function (req, res) {   
-  ClientCurrentPosition = req.body
+//return all connected VR apps
+app.get('/clients-list', function (req, res) {
+	res.set('Access-Control-Allow-Origin', '*');
+	var VRApps = [];
+	for(let [ip, data] of vr_side_apps){
+		VRApps.push(ip);
+    }
+	return res.status(200).json(VRApps)
+})
+
+
+app.post('/client-infos', function (req, res) {
+	var ip = getIp(req);
+		 
+	//ClientCurrentPosition = req.body
+	
+	if(!vr_side_apps.has(ip)){
+		console.log("NEW CONNECTION FROM VR APP:", ip)
+	}
+	vr_side_apps.set(ip,{ClientCurrentPosition:req.body,Commands:[],timeSinceUpdate:0})
+
+
+
+  
   /*console.log(
     "CLIENT",
     JSON.stringify(        
@@ -39,9 +93,14 @@ app.get('/client', function (req, res) {
 
 app.get('/client', function (req, res) {    
     // if the array contains at least one element
-    if (Commands.length > 0) {
+	var appID = req.body.appid;
+	if(!vr_side_apps.has(appID)){
+		return res.status(404).end();
+	}
+	
+    if (vr_side_apps[appID].Commands.length > 0) {
         // Pop the last element ( get last element, then remove from the array)
-        const command = Commands.shift()
+        const command = vr_side_apps[appID].Commands.shift()  //pop()
         // send the command
         
         console.log(
@@ -63,25 +122,37 @@ app.get('/client', function (req, res) {
 
 // Endpoint /client
 // For every request, store a new command
-app.post('/seller', function (req, res) {   
-  res.set('Access-Control-Allow-Origin', '*');
-  if (req.body.command === "Teleport") {
-    console.log(
-      "PUSHING",
-      JSON.stringify(        
-        req.body, null, 2
-      )
-    )
-
-    req.body.x = parseInt(req.body.x)
-    req.body.y = parseInt(req.body.y)
-    req.body.z = parseInt(req.body.z)
-    req.body.rotation = parseInt(req.body.rotation)
-
-    Commands.push(req.body)
+app.post('/seller', function (req, res) {
+	res.set('Access-Control-Allow-Origin', '*');
+	var appID = req.body.appid;
+	delete req.body.appid;
+	
+	if(!vr_side_apps.has(appID)){
+		return res.status(404).end();
+	}
+	
+	console.log("REQUEST FROM SELLER:",getIp(req))
+	
+	if (req.body.command === "Teleport") {
+		console.log("PUSHING",JSON.stringify(req.body, null, 2))
+		req.body.x = parseInt(req.body.x)
+		req.body.y = parseInt(req.body.y)
+		req.body.z = parseInt(req.body.z)
+		req.body.rotation = parseInt(req.body.rotation)
+		
+		vr_side_apps[appID].Commands.push(req.body)
+	
+	}
+  
+  if (req.body.command === "ChangeStyle"){
+	  console.log("PUSHING",JSON.stringify(req.body, null, 2))
+	  vr_side_apps[appID].Commands.push(req.body)
   }
+  
   res.status(200).end()
 })
+
+
 
 
 app.listen(app.get('port'), function() {
